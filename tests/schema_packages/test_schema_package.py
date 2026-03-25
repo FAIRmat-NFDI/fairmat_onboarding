64ef62a7950db711140562e38ded6950ecc0ef0e
Ahmed Mansour <ahm531@gmail.com>
2026-03-25 14:40:35 +0100

    Add fairmat_onboarding submodule with URL configuration

--- tests/schema_packages/test_schema_package.py
+++ tests/schema_packages/test_schema_package.py
@@ -8,4 +8,12 @@ def test_schema_package():
     entry_archive = parse(test_file)[0]
     normalize_all(entry_archive)
 
-    assert entry_archive.data.message == 'Hello Markus!'
+    data = entry_archive.data
+    assert data.pi_name == 'Dr. Jane Doe'
+    assert 'Humboldt-Universität zu Berlin' in data.institutions
+    assert data.research_group == 'Computational Materials Science Group'
+    assert 'Area C - Computation' in data.fairmat_areas
+    assert data.RDM_contact_email == 'john.smith@hu-berlin.de'
+    assert data.research_focus.research_type == 'Computational'
+    assert data.research_data_management.using_nomad == 'Yes'
+    assert 'Plugin development' in data.research_data_management.training_topics
